baa436f271cb5331eeccde43127be69bb2fa2202
Jamie <u1854720@unimail.hud.ac.uk>
2020-10-25 23:31:08 +0000

    Initial Commit

--- src/main/java/com/scrabble/scrabbleScoreTracker/controller/HomeScreenController.java
+++ src/main/java/com/scrabble/scrabbleScoreTracker/controller/HomeScreenController.java
@@ -1,36 +1,38 @@
 package com.scrabble.scrabbleScoreTracker.controller;
 
 import com.scrabble.scrabbleScoreTracker.database.MemberDataService;
-import com.scrabble.scrabbleScoreTracker.members.Member;
 import com.scrabble.scrabbleScoreTracker.members.MemberStorage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.security.Principal;
 import java.sql.SQLException;
 
 @Controller
 public class HomeScreenController {
 
-
     @GetMapping("/")
-    public String splashScreen(){
-        return "splashScreen";
+    public RedirectView redirectUser(){
+        return new RedirectView("home");
     }
 
+
     @GetMapping("/home")
-    public String home(String name, Model model) throws SQLException {
+    public String home(){
         return "home";
     }
 
-    @GetMapping("/members")
+    @GetMapping("/allMembers")
     public String members(Model model) throws SQLException {
-        model.addAttribute("users", MemberStorage.getAllMemberDetails());
         MemberDataService.getAllMemberDataFromDb();
+        model.addAttribute("users", MemberStorage.getAllMemberDetails());
         return "allMembers";
+    }
+
+    @GetMapping("/Scoreboard")
+    public String scoreboard(){
+        return "scoreboard";
     }
 
 
